ecc05ae6e56cc6cc0b0a82f89eec903df3801da8
Michał Rokita <mrokita@mrokita.pl>
2024-07-24 11:47:01 +0200

    feat: implement basic stream processing functions

--- pybrook-redismodule/README.md
+++ pybrook-redismodule/README.md
@@ -9,4 +9,22 @@ Registers an input stream, usually the id is the same as the input stream
 This enables tagging with message ids, which are in the following form: `<obj_id>:<obj_message_id>`, where `<obj_id>` is derived from the `<obj_id_field>`, 
 and `<obj_message_id>` is an incremental counter bound to the corresponding object.
 
-`BROOK.REGISTER_DEPENDENCIES <count>`
+`BROOK.REGISTER_DEPENDENCIES <project> <out_stream> <N> <dep_stream_1> <dep_key_1> ... <dep_stream_N> <dep_key_N>`
+
+Registers a list of dependencies, incl their input streams. `out_stream` must be unique for `project`, otherwise will be erased
+<!-- TODO: add historical deps -->
+
+`BROOK.REMOVE_ABANDONED_DEPENDENCIES <project> <out_stream_1> <out_stream_N>`
+
+
+# Core concepts
+
+- Streams should be used only for Artificial Fields, Input Reports and Output Reports, meaning:
+
+
+For every Input Report, a Message ID is first calculated.
+Then all the required fields are sent to corresponding Dependency Resolver HMAPS directly, without an intermediate stream.
+If the HMAP has the LENGTH of DEPS_NO, it is sent to an argument stream, which then received by a PyBrook consumer group. 
+
+Thus, a dependency resolver is just a function called by the redis module internally, when a Stream Event occurs,
+caused either by an Artificial Field generation or by Input Report arrival.
--- pybrook-redismodule/src/lib.rs
+++ pybrook-redismodule/src/lib.rs
@@ -1,30 +1,43 @@
 use std::ptr::NonNull;
-use redis_module::{redis_module, Context, RedisError, RedisResult, RedisString, NextArg, KeyType, RedisModuleStreamID, RedisValue, NotifyEvent, RedisModule_StreamAdd, raw, REDISMODULE_STREAM_ADD_AUTOID, RedisModuleKey, RedisModuleString};
+use redis_module::{redis_module, Context, RedisError, RedisResult, RedisString, NextArg, KeyType, RedisModuleStreamID, RedisValue, NotifyEvent, RedisModule_StreamAdd, raw, REDISMODULE_STREAM_ADD_AUTOID, RedisModuleKey, RedisModuleString, Status};
 use redis_module::key::{KeyFlags, KeyMode, RedisKey};
 use std::io::Read;
+use serde_json::Map;
+use std::os::raw::c_int;
+use serde_json::json;
 
 fn redis_string<T: Into<Vec<u8>>>(ctx: &Context, value: T) -> RedisString{
     RedisString::create(NonNull::new(ctx.ctx), value)
 }
 
-fn stream_add(ctx: &Context, key_name: &[u8]){
+fn stream_add(ctx: &Context, key_name: &[u8], message: serde_json::Value){
     let mut id = RedisModuleStreamID{ ms: 0, seq: 0 };
-    let raw = &mut id as *mut RedisModuleStreamID;
-    let vals = [redis_string(ctx, "A"), redis_string(ctx, "B")];
-    let mut args = vals.iter()
+    let stream_message_id = &mut id as *mut RedisModuleStreamID;
+
+    let mut message_vector: Vec<RedisString> = vec![];
+    for (key, value) in message.as_object().unwrap() {
+        message_vector.push(redis_string(ctx, key.as_str()));
+        message_vector.push(redis_string(ctx, value.as_str().unwrap())); // what about int, float, etc?
+    }
+    let mut args = message_vector.iter()
                 .map(|v| v.inner)
                 .collect::<Vec<_>>();
 
-    let key = raw::open_key(ctx.ctx, redis_string(ctx, "test").inner, raw::KeyMode::WRITE);
-    let res = unsafe {
-            raw::RedisModule_StreamAdd.unwrap()(
+    let key = raw::open_key(ctx.ctx, redis_string(ctx, key_name).inner, raw::KeyMode::WRITE);
+    let status: Status = unsafe {
+            RedisModule_StreamAdd.unwrap()(
                 key,
-                1,
-                raw,
+                REDISMODULE_STREAM_ADD_AUTOID as c_int,
+                stream_message_id,
                 (&mut args).as_mut_ptr(),
-                1
+                (message_vector.len() as i64) / 2
             )
-        };
+    }.into();
+    match status {
+        Status::Ok => {}
+        Status::Err => {panic!("err")}
+    }
+    raw::close_key(key);
 }
 
 fn stream_read_from(ctx: &Context, args: Vec<RedisString>) -> RedisResult {
@@ -78,15 +91,36 @@ fn hello_mul(_: &Context, args: Vec<RedisString>) -> RedisResult {
 
 
 fn on_stream(ctx: &Context, event_type: NotifyEvent, event: &str, key: &[u8]) {
+    println!("{:?} {} {:?}", event_type, event, key);
     let stream = ctx.open_key(&RedisString::create(NonNull::new(ctx.ctx), key));
-    stream_add(ctx, key);
-    stream_add(ctx, key);
-    let iter = stream.get_stream_iterator(false);
-    let key_type = stream.key_type();
+    stream_add(ctx, "out_test".as_bytes(), json!({"a": "b", "c": "d"}));
+    for record in stream.get_stream_iterator(false).unwrap() {
+        let mut message = serde_json::Map::new();
+        for field in record.fields {
+            println!("FIELD {:?} {:?}", field.0.to_string(), field.1.to_string());
+            let value: serde_json::Value = serde_json::from_str(field.1.to_string().as_str()).unwrap();
+            message.insert(field.0.to_string(), value);
+
+        }
+        println!("{:?}", message)
+    }
 }
 
 //////////////////////////////////////////////////////
+#[cfg(test)]
+mod tests {
+    use std::str::FromStr;
+    use serde_json::Value;
+    use super::*;
 
+    #[test]
+    fn it_works() {
+        let val: Value = serde_json::from_str(String::from_str("\"x\"").unwrap().as_str()).unwrap();
+
+    }
+}
+
+#[cfg(not(test))]
 redis_module! {
     name: "hello",
     version: 1,
@@ -95,7 +129,7 @@ redis_module! {
     commands: [
         ["hello.mul", hello_mul, "", 0, 0, 0],
     ],
-    // event_handlers: [
-    //     [@STREAM: on_stream],
-    // ]
+    event_handlers: [
+        [@STREAM: on_stream],
+    ]
 }
