09b8103ec6dc7f77bb0071a92fa3a148432c4435
Michał Rokita <mrokita@mrokita.pl>
2024-09-07 17:39:31 +0200

    tests: add a performance test

--- tests/test_redismodule.py
+++ tests/test_redismodule.py
@@ -1,3 +1,7 @@
+import multiprocessing.pool
+from itertools import chain
+from time import time, sleep
+
 import redis
 
 from pybrook.config import MSG_ID_FIELD
@@ -7,7 +11,18 @@ from pybrook.redis_plugin_integration import (
     DependencyField,
     Dependency,
 )
+from tests.conftest import TEST_REDIS_URI
 
+conn = None
+
+
+def init_pool():
+    global conn
+    conn = redis.from_url(TEST_REDIS_URI, decode_responses=True)
+
+
+def xadd(*args):
+    conn.xadd(*args)
 
 def test_resolver(redis_sync: redis.Redis):
     redis_sync.execute_command(
@@ -30,8 +45,17 @@ def test_resolver(redis_sync: redis.Redis):
             }
         ).json(),
     )
-    redis_sync.xadd("stream_a", {MSG_ID_FIELD: '"1:1"', "x": "1"})
-    redis_sync.xadd("stream_b", {MSG_ID_FIELD: '"1:1"', "y": "2"})
+    redis_sync.xadd("stream_a", {MSG_ID_FIELD: '"1:0"', "x": "1"})
+    redis_sync.xadd("stream_b", {MSG_ID_FIELD: '"1:0"', "y": "2"})
+
+    REQUESTS = 300000
+    args = list(chain(*((("stream_a", {MSG_ID_FIELD: f'"1:{x}"', "x": "1"}), ("stream_b", {MSG_ID_FIELD: f'"1:{x}"', "y": "2"})) for x in range(1, REQUESTS))))
     assert [m for _, m in redis_sync.xrevrange("out", "+", "-")] == [
-        {"@pb@msg_id": '"1:1"', "x_dest": "1", "y_dest": "2"}
+        {"@pb@msg_id": '"1:0"', "x_dest": "1", "y_dest": "2"}
     ]
+
+    with multiprocessing.Pool(processes=16, initializer=init_pool) as pool:
+        t = time()
+        list(pool.starmap(xadd, args))
+    assert redis_sync.xlen("out") == REQUESTS
+    print("RPS achieved: ", REQUESTS / (time() - t))
